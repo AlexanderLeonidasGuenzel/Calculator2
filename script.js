81af057dc5c0d4f06df78c6ee8cc644621678d1b
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2025-12-08 03:13:07 +0100

    refactoring: handling number 0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const display = document.querySelector(".display");
 const keys = document.querySelectorAll(".keys");
 const powerBtn = document.querySelector("#power");
+let calculationString = "";
 
 document.addEventListener("DOMContentLoaded", () => {
   powerBtn.addEventListener("click", power);
@@ -45,14 +46,13 @@ function themeEventListener() {
   );
 }
 
-let calculationString = "";
-
 function inputHandler(event) {
   const dataItem = event.target.dataset.item;
   const OPERATOR = /[\+\-\*\/]/;
   const DELETE = /delete/;
   const CLEAR = /clear/;
   const DIGIT = /\d/;
+  const DIGITNONULL = /[1-9]/;
   const POINT = /\./;
   const RESULT = /=/;
 
@@ -65,39 +65,66 @@ function inputHandler(event) {
     reset();
   }
 
-  if (DIGIT.test(dataItem)) {
-    view(dataItem, display.value);
-    calculationString += dataItem;
-  }
-
-  if (POINT.test(dataItem)) {
-    let e = calculationString.split(OPERATOR).pop();
-    if (e.includes(".") === false) {
-      view(dataItem, display.value);
-      calculationString += dataItem;
-    }
-  }
-
-  if (OPERATOR.test(dataItem)) {
-    let e = calculationString.slice(-1);
-    if (!OPERATOR.test(e)) {
-      console.log(dataItem);
-      calculationString += dataItem;
-      if (dataItem === "*") {
-        view("x", display.value);
-      } else if (dataItem === "/") {
-        view("÷", display.value);
-      } else {
-        view(dataItem, display.value);
-      }
-    }
-  }
-
   if (RESULT.test(dataItem)) {
     let result = calculation();
     calculationString = result.toString();
     view(result);
   }
+
+  //append
+  if (display.value.length <= 30) {
+    if (DIGIT.test(dataItem)) {
+      let number = display.value.split(OPERATOR).pop();
+      if (dataItem === "0") {
+        if (display.value === "" || OPERATOR.test(display.value.slice(-1))) {
+          view(dataItem + ".", display.value);
+          calculationString += dataItem + ".";
+        } else if (number.includes(".")) {
+          view(dataItem, display.value);
+          calculationString += dataItem;
+        } else if (DIGITNONULL.test(number)) {
+          view(dataItem, display.value);
+          calculationString += dataItem;
+        }
+      } else {
+        if (display.value.slice(-1) === "0" && !DIGITNONULL.test(number)) {
+          view(dataItem, slice(0, -1));
+          calculationString = calculationString.slice(0, -1) + dataItem;
+        } else {
+          view(dataItem, display.value);
+          calculationString += dataItem;
+        }
+      }
+    }
+
+    if (POINT.test(dataItem)) {
+      let e = calculationString.split(OPERATOR).pop();
+      if (e.includes(".") === false) {
+        if (e.slice(-1) === "") {
+          view("0" + dataItem, display.value);
+          calculationString += "0" + dataItem;
+        } else {
+          view(dataItem, display.value);
+          calculationString += dataItem;
+        }
+      }
+    }
+
+    if (OPERATOR.test(dataItem)) {
+      let e = calculationString.slice(-1);
+      if (!OPERATOR.test(e)) {
+        calculationString += dataItem;
+        if (dataItem === "*") {
+          view("x", display.value);
+        } else if (dataItem === "/") {
+          view("÷", display.value);
+        } else {
+          view(dataItem, display.value);
+        }
+      }
+    }
+  }
+  console.log("calculationString: " + calculationString);
 }
 
 /* View help functions*/
