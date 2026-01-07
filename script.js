f732c72b8e594e3f75cd8cee3a04daf8b788d192
AlexanderGuenzel <alexanderleonidasguenzel@gmail.com>
2026-01-07 05:36:24 +0100

    refactor script

--- script.js
+++ script.js
@@ -1,164 +1,172 @@
-const OPERATOR = /[\+\-\*\/]/;
-const DELETE = /delete/;
-const CLEAR = /clear/;
-const DIGIT = /\d/;
-const DIGIT_1_TO_9 = /[1-9]/;
-const POINT = /\./;
-const RESULT = /=/;
-const MATH_ERROR = "Math Error";
-const display = document.querySelector(".display");
-const keys = document.querySelectorAll(".keys");
+const keysField = document.querySelectorAll(".keys-field");
 const powerBtn = document.querySelector("#power");
-let calculationString = "";
+const display = document.querySelector(".display");
 
-document.addEventListener("DOMContentLoaded", () => {
-  powerBtn.addEventListener("click", power);
-  reset();
-  menuEvent();
-  themeEventListener();
-});
+const MATH_ERROR = "Math Error";
 
-function inputHandler(event) {
-  const dataItem = event.target.dataset.item;
+let state = {
+  expression: "",
+  poweredOn: false,
+};
 
-  if (display.value === MATH_ERROR) {
-    view("");
-  }
+document.querySelector(".keys-field").addEventListener("click", inputHandler);
+handleClear();
 
-  if (DELETE.test(dataItem)) {
-    view(slice(0, -1));
-    calculationString = calculationString.slice(0, -1);
-  }
-
-  if (CLEAR.test(dataItem)) {
-    reset();
-  }
-
-  if (RESULT.test(dataItem)) {
-    let result = calculation();
-    if (result === Infinity || result === -Infinity) {
-      view(MATH_ERROR);
-      calculationString = "";
-    } else {
-      calculationString = result.toString();
-      view(result);
-    }
-  }
-
-  if (display.value.length <= 30) {
-    let displayIsEmpty = display.value === "";
-    let lastNumber = calculationString.split(OPERATOR).pop();
-    let lastElementisOperator = OPERATOR.test(calculationString.slice(-1));
-    let numberHasOtherDigits = DIGIT_1_TO_9.test(lastNumber);
-    let numberHasPoint = lastNumber.includes(".");
-
-    if (DIGIT.test(dataItem)) {
-      if (dataItem === "0") {
-        if (displayIsEmpty || lastElementisOperator) {
-          view(dataItem, display.value);
-          calculationString += dataItem;
-        } else if (numberHasPoint || numberHasOtherDigits) {
-          view(dataItem, display.value);
-          calculationString += dataItem;
-        }
-      } else {
-        view(dataItem, display.value);
-        calculationString += dataItem;
-      }
-    }
-
-    if (POINT.test(dataItem)) {
-      let e = calculationString.split(OPERATOR).pop();
-      if (e.includes(".") === false) {
-        if (e.slice(-1) === "") {
-          view("0" + dataItem, display.value);
-          calculationString += "0" + dataItem;
-        } else {
-          view(dataItem, display.value);
-          calculationString += dataItem;
-        }
-      }
-    }
-
-    if (OPERATOR.test(dataItem)) {
-      if (lastElementisOperator === false) {
-        calculationString += dataItem;
-        //view
-        if (dataItem === "*") {
-          view("\u0078", display.value);
-        } else if (dataItem === "/") {
-          view("\u00F7", display.value);
-        } else {
-          view(dataItem, display.value);
-        }
-      }
-    }
-  }
-  console.log("calculationString: " + calculationString);
-}
-
-function calculation() {
-  return Number.parseFloat(Number(eval(calculationString)).toFixed(9));
-}
-
-function view(item, actualView) {
-  if (typeof actualView === "undefined") {
-    if (item === "") {
-      display.value = "";
-    } else {
-      display.value = item;
-    }
-  } else {
-    display.value = actualView + item;
-  }
-}
-
-function slice(start, end) {
-  return display.value.slice(start, end);
-}
-
-function reset() {
-  view("");
-  calculationString = "";
-  console.clear();
-}
-
-function power() {
+function powerSwitch() {
   if (display.classList.contains("on")) {
-    keys.forEach((b) => b.removeEventListener("click", inputHandler));
-    reset();
+    state.poweredOn = false;
+    handleClear();
     display.placeholder = "";
   } else {
-    keys.forEach((b) => b.addEventListener("click", inputHandler));
-    view("");
+    state.poweredOn = true;
+    setDisplay();
     display.placeholder = "0";
   }
   display.classList.toggle("on");
 }
 
-function menuEvent() {
-  const menuText = document.querySelector(".menu-text");
-  menuText.addEventListener("click", function () {
-    const menu = document.querySelector(".menu");
-    menu.classList.toggle("open");
-    if (menu.classList.contains("open")) {
-      menuText.textContent = "close";
-    } else {
-      menuText.textContent = "Menu";
-    }
-  });
+// Input Handler
+function inputHandler(e) {
+  const item = e.target.dataset.item;
+
+  if (item === "power") {
+    powerSwitch();
+  }
+
+  console.log("poweredOn: " + state.poweredOn);
+  if (state.poweredOn === false) return;
+
+  if (display.value === MATH_ERROR) {
+    setDisplay();
+  }
+
+  if (isClear(item)) return handleClear();
+  if (isDelete(item)) return handleDelete();
+  if (isDigit(item)) return handleDigit(item);
+  if (isPoint(item)) return handlePoint();
+  if (isOperator(item)) return handleOperator(item);
+  if (isResult(item)) return handleResult();
 }
 
-function themeEventListener() {
-  const galaxyTheme = document.getElementById("theme-galaxy");
-  const item = document.querySelectorAll(".menu-item");
-  item.forEach((i) =>
-    i.addEventListener("click", () => {
-      if (i.id === "galaxy") {
-        galaxyTheme.disabled = false;
-      } else {
-        galaxyTheme.disabled = true;
-      }
-    })
-  );
+// Handler
+function handleDigit(digit) {
+  if (digit === "0") {
+    if (hasLeadingZero() && hasNoOtherDigits() && hasNoPoint()) {
+      return;
+    }
+  }
+
+  if (hasLeadingZero() && hasNoPoint()) {
+    handleDelete();
+  }
+  appendToDisplay(digit);
+  state.expression += digit;
+}
+
+function handlePoint() {
+  if (hasPoint()) return;
+
+  if (isEmptyNumber()) {
+    state.expression += "0.";
+    appendToDisplay("0.");
+  } else {
+    state.expression += ".";
+    appendToDisplay(".");
+  }
+}
+
+function handleOperator(operator) {
+  if (lastCharIsOperator()) return;
+
+  state.expression += operator;
+
+  if (operator === "*") {
+    appendToDisplay("x");
+  } else if (operator === "/") {
+    appendToDisplay("÷");
+  } else {
+    appendToDisplay(operator);
+  }
+}
+
+function handleResult() {
+  let result = Number.parseFloat(Number(eval(state.expression)).toFixed(9));
+
+  if (result === Infinity || result === -Infinity) {
+    setDisplay(MATH_ERROR);
+    state.expression = "";
+  } else {
+    state.expression = result.toString();
+    setDisplay(result);
+  }
+}
+
+function handleDelete() {
+  setDisplay(display.value.slice(0, -1));
+  state.expression = state.expression.slice(0, -1);
+}
+
+function handleClear() {
+  setDisplay();
+  state.expression = "";
+}
+
+function setDisplay(value = "") {
+  display.value = value;
+}
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+//Helper
+function isClear(value) {
+  return /clear/.test(value);
+}
+
+function isDelete(value) {
+  return /delete/.test(value);
+}
+
+function isDigit(value) {
+  return /\d/.test(value);
+}
+
+function isPoint(value) {
+  return value === ".";
+}
+
+function isOperator(value) {
+  return /[\+\-\*\/]/.test(value);
+}
+
+function isResult(value) {
+  return value === "=";
+}
+
+function lastCharIsOperator() {
+  return isOperator(state.expression.slice(-1));
+}
+
+function hasNoOtherDigits() {
+  return /[1-9]/.test(lastNumber()) === false;
+}
+
+function hasLeadingZero() {
+  return lastNumber().charAt(0) === "0";
+}
+
+function isEmptyNumber() {
+  return lastNumber().charAt(0) === "";
+}
+
+function hasNoPoint() {
+  return lastNumber().includes(".") === false;
+}
+function hasPoint() {
+  return lastNumber().includes(".") === true;
+}
+
+function lastNumber() {
+  return state.expression.split(/[\+\-\*\/]/).pop();
 }
